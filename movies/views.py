fabae539cddc83501c919ddddb47a29d4fbf501d
João Victor Ramires Guimarães Brito <joaoramires.engsoft@gmail.com>
2020-02-06 19:52:30 -0300

    Feature: Use DB Abstraction API to display movies in movies route

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    movies = Movie.objects.all()
+    output = ', '.join([m.title for m in movies])
+    return HttpResponse(output)

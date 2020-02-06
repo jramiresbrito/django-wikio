56c162dfd80cc9f583b2500a9cb62bacb75ba4f0
João Victor Ramires Guimarães Brito <joaoramires.engsoft@gmail.com>
2020-02-06 19:39:44 -0300

    Feature: Customize Admin

--- movies/admin.py
+++ movies/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Genre, Movie
 
-admin.site.register(Genre)
-admin.site.register(Movie)
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'number_in_stock', 'daily_rate')
+    exclude = ('date_created', )
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)

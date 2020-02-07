7e822d859cf6c44b344c591f3b6161f4c7648211
João Victor Ramires Guimarães Brito <joaoramires.engsoft@gmail.com>
2020-02-06 22:48:14 -0300

    Feature: ADD Homepage

--- django_wikio/urls.py
+++ django_wikio/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from api.models import MovieResource
+from . import views
 
 movie_resource = MovieResource()
 
 urlpatterns = [
+    path('', views.home),
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
     path('api/', include(movie_resource.urls))

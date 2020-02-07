43524b5b3967c8964b432655424e39d9b48667c7
João Victor Ramires Guimarães Brito <joaoramires.engsoft@gmail.com>
2020-02-06 22:30:23 -0300

    Feature: ADD Movies API

--- django_wikio/urls.py
+++ django_wikio/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from api.models import MovieResource
+
+movie_resource = MovieResource()
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
+    path('api/', include(movie_resource.urls))
 ]
